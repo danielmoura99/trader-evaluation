e1c8566dadf805e680aac4c1f708d1511e25d811
dsmouraa <dsmouraa@icloud.com>
2024-12-14 10:45:00 -0300

    feat: changed the payload to lambda

---
+++ utils/broker-config.ts
@@ -0,0 +1,7 @@
+// utils/broker-config.ts
+export const BROKER_CONFIG = {
+  authenticationCode: "2B295526980F475BA2A608C8C1C4F8DF",
+  testAccount: "1000380752",
+  apiGatewayUrl:
+    "https://m043hg2via.execute-api.sa-east-1.amazonaws.com/prod/test",
+};
